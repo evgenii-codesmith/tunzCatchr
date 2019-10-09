f327b50048e5033e3e1dcc4c9f6b8a86448ba779
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-10-09 20:13:51 +0300

    CRUD functionality upgarded

--- app.py
+++ app.py
@@ -31,7 +31,41 @@ def index():
         
     else:
         all_tunes = Tune.query.order_by(Tune.date_created).all()
-        return render_template('index.html', all_tunes=all_tunes)
+        return render_template('index.html', tunes=all_tunes)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    target_tune = Tune.query.get_or_404(id)
+    
+    try:
+        db.session.delete(target_tune)
+        db.session.commit()
+        return redirect('/')
+        
+    except:
+        return 'Unable to delete tune %s' % id
+
+@app.route('/update/<int:id>',methods = ['POST', 'GET'])
+def update(id):
+    target_tune = Tune.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        target_tune.content = request.form['new_content']
+        downloaded = request.form['downloaded']
+
+        if downloaded == 'Yes':
+            target_tune.downloaded = True
+        elif downloaded == 'No':
+            if target_tune.downloaded:
+                target_tune.downloaded = False
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Unable to update'
+    else:
+        return render_template('update.html', tune=target_tune)
 
 if __name__=='__main__':
     app.run(debug=True)
