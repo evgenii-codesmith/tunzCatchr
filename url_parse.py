5c2c5749a55f864eaf17ddc5cafc8ac59758240d
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-10-11 03:07:10 +0300

    Url_parse func edited

--- url_parse.py
+++ url_parse.py
@@ -1,23 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-# youtube_url = 'https://www.youtube.com/watch?v=GoFQHjneFFI'
-
-# response = requests.get(youtube_url)
-# page = response.text
-# soup = BeautifulSoup(page,'html.parser')
-# title = soup.select('#eow-title')[0].text.strip()       
-# title = title.split('-')
-# artist = title[0].strip()
-# tune = title[1].strip()
-
-# # Removing [OUT NOW] from a tune name
-# index = tune.find('[OUT NOW]') 
-# if index > 0:
-#     tune = tune[:index]
-
-# print('Artist: ',artist, 'Tune:', tune)
 def get_tune_data(url):
+    # youtube_url = 'https://www.youtube.com/watch?v=GoFQHjneFFI'
+    
+    # youtube_url = url
+    # response = requests.get(youtube_url)
+    # page = response.text
+    # soup = BeautifulSoup(page,'html.parser')
+    # title = soup.select('#eow-title')[0].text.strip()       
+    # title = title.split('-')
+    # artist = title[0].strip()
+    # tune_name = title[1].strip()
+
+    # # Removing [OUT NOW] from a tune name
+    # index = tune_name.find('[OUT NOW]') 
+    # if index > 0:
+    #     tune_name = tune_name[:index]
+
+    # print('Artist: ',artist, 'Tune:', tune)
+    
     artist = 'Rolo Green'
     tune_name = 'Cool track (Original Mix)'  
     return(artist, tune_name)
