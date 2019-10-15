40ce8762b7070be683ec2285e25334dc438e5172
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-10-16 00:29:01 +0300

    Sync download to folder

--- file_download.py
+++ file_download.py
@@ -6,8 +6,11 @@ def get_file_from_youtube(url, file_format, audio_quality):
     audio_quality: String: value from '0'(better) to '9'(worse) or specific bitrate ie '128K'
     See youtube-dl -h for more info.
     """
+    
+    output_template = "./Downloaded/%(title)s.%(ext)s"
     proc = subprocess.Popen(
-        ["youtube-dl","--extract-audio","--audio-format",file_format, "--audio-quality",audio_quality, url],
+        ["youtube-dl","--extract-audio","--audio-format",file_format,
+         "--audio-quality", audio_quality, "-o", output_template, url,],
         stdout=subprocess.PIPE,stderr=subprocess.PIPE,) 
     
     # output, errors = proc.communicate()
