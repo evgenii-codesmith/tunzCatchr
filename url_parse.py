d72b7ce80503f1c52876ad381e0409128f1bfa74
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-11-18 21:31:55 +0300

    Tags parsing: 'no artist' fixed

--- url_parse.py
+++ url_parse.py
@@ -1,17 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_tune_data(url):
     response = requests.get(url)
     page = response.text
-    soup = BeautifulSoup(page,'html.parser')
-    title = soup.select('#eow-title')[0].text.strip()       
-    title = title.split('-')
-    artist = title[0].strip()
-    tune_name = title[1].strip()
+    soup = BeautifulSoup(page, 'html.parser')
+    title = soup.select('#eow-title')[0].text.strip()
+
+    if title.find('-') != -1:
+        title = title.split('-')
+        artist = title[0].strip()
+        tune_name = title[1].strip()
+    else:
+        artist = 'Unknown'
+        tune_name = title.strip()
 
     # Removing [OUT NOW] from a tune name
-    index = tune_name.find('[OUT NOW]') 
+    index = tune_name.find('[OUT NOW]')
     if index > 0:
         tune_name = tune_name[:index]
 
